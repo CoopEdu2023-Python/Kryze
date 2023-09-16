2cfc99becfef66cdd95f94880c404f82c7139ca5
KryzeZZZ <mc200710@outlook.com>
2023-09-16 13:10:25 +0800

    test

---
+++ Unit1/Chapter2/1.2.1.py
@@ -0,0 +1,6 @@
+ans_1 = 2 + 6
+ans_2 = 2 * 4
+ans_3 = 10 - 2
+ans_4 = 16 / 2
+print(type(ans_1), type(ans_2), type(ans_3), type(ans_4))
+print(ans_1, ans_2, ans_3, ans_4)
---
+++ Unit1/Chapter2/1.2.2.py
@@ -0,0 +1,7 @@
+var_1 = 1
+var_2 = 2
+var_3 = var_2
+var_4 = var_3
+var_5 = var_3 - var_2 + var_4
+var_1 = var_5
+print(var_1)
