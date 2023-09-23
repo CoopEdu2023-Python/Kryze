c6b6d9ed23339b11a7cb751ecd9cb1eb9b893a22
KryzeZZZ <mc200710@outlook.com>
2023-09-23 11:24:05 +0800

    test

--- Unit1/PA/main.py
+++ Unit1/PA/main.py
@@ -32,36 +32,58 @@ y = 0
 i = 0
 flag = 0
 os.system('cls')
+p = [100, 100]
+g = 0
 # 主要部分
-while flag == 0:
-    clear_screen()
-    if ir == 1:
-        print("非法输入，请重新输入")
-    bk = f"{list[0][0]}  | {list[0][1]} |  {list[0][2]}\n___|___|___\n   |   |   \n{list[1][0]}  | {list[1][1]} |  {list[1][2]}\n___|___|___\n   |   |   \n{list[2][0]}  | {list[2][1]} |  {list[2][2]}"
-    print(bk)
-    print(f"现在是{tp[i % 2]}")
-    xx = input("输入棋子X轴值（从1开始）")
-    yy = input("输入棋子Y轴值（从1开始）")
-    x = int(xx) - 1
-    y = int(yy) - 1
-    if ((x < 0) or (x > 2)) or ((y < 0) or (y > 2)):
-        ir = 1
+while p[0] > 0 and p[1] > 0:
+    print(f"当前积分:\n玩家1 {p[0]}\n玩家2 {p[1]}")
+    g = int(input("请输入一局所需积分:"))
+    if g > p[0] or g > p[1]:
+        clear_screen()
+        print("非法输入,请重试")
         continue
-    if list[x][y] == ' ':
-        list[x][y] = tp[i % 2]
-        ir = 0
-    else:
-        ir = 1
-        continue
-    i += 1
-    flag = win(list, x, y)
-    # 特殊情况判断
-    if flag == 1:
-        print(f"{tp[(i - 1) % 2]}赢了")
+    while flag == 0:
+        clear_screen()
+        if ir == 1:
+            print("非法输入，请重新输入")
+        else:
+            print(f"当前积分:\n玩家1 {p[0]}\n玩家2 {p[1]}")
         bk = f"{list[0][0]}  | {list[0][1]} |  {list[0][2]}\n___|___|___\n   |   |   \n{list[1][0]}  | {list[1][1]} |  {list[1][2]}\n___|___|___\n   |   |   \n{list[2][0]}  | {list[2][1]} |  {list[2][2]}"
         print(bk)
-        break
-    if (i == 9) and (flag == 0):
-        print("平局")
-        break
-
+        print(f"现在是{tp[i % 2]}")
+        xx = input("输入棋子X轴值（从1开始）")
+        yy = input("输入棋子Y轴值（从1开始）")
+        x = int(xx) - 1
+        y = int(yy) - 1
+        if ((x < 0) or (x > 2)) or ((y < 0) or (y > 2)):
+            ir = 1
+            continue
+        if list[x][y] == ' ':
+            list[x][y] = tp[i % 2]
+            ir = 0
+        else:
+            ir = 1
+            continue
+        i += 1
+        flag = win(list, x, y)
+        # 特殊情况判断
+        if flag == 1:
+            print(f"{tp[(i - 1) % 2]}赢了")
+            p[(i - 1) % 2] += g
+            p[i % 2] -= g
+            bk = f"{list[0][0]}  | {list[0][1]} |  {list[0][2]}\n___|___|___\n   |   |   \n{list[1][0]}  | {list[1][1]} |  {list[1][2]}\n___|___|___\n   |   |   \n{list[2][0]}  | {list[2][1]} |  {list[2][2]}"
+            print(bk)
+            input('输入任意键继续')
+            clear_screen()
+            break
+        if (i == 9) and (flag == 0):
+            print("平局")
+            break
+if p[0] <= 0:
+    print('玩家1赢了')
+    input('输入任意键继续')
+    clear_screen()
+else:
+    print('玩家2赢了')
+    input('输入任意键继续')
+    clear_screen()
