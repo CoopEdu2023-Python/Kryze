04b96cec3d35264b789a99e284bfda67f85864f7
KryzeZZZ <mc200710@outlook.com>
2023-10-26 16:02:44 +0800

    test

--- Exam/Exam3/3.py
+++ Exam/Exam3/3.py
@@ -10,9 +10,9 @@ def find_sum_of_3(num_list: list) -> list[tuple]:
             if sum == 0:
                 answers.append(tuple(sorted([num_list[i], num_list[left], num_list[right]])))
                 # 跳过重复
-                while left < right and num_list[left] == num_list[left + 1]:
+                while num_list[left] == num_list[left + 1]:
                     left += 1
-                while left < right and num_list[right] == num_list[right - 1]:
+                while num_list[right] == num_list[right - 1]:
                     right -= 1
                 left += 1
                 right -= 1
@@ -23,6 +23,6 @@ def find_sum_of_3(num_list: list) -> list[tuple]:
     return answers
 
 
-num_list = [-1, 0, 1, 2, -1, -4, 0, 2, 0, 4, -4, -2, -1, 2]
+num_list = [1, 1, 1, 1, -1, 0]
 num_list.sort()
 print(find_sum_of_3(num_list))
