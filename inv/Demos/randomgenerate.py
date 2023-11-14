76273d5ce775a809b7e2c0d2bb80017c28454464
mc200 <mc200710@outlook.com>
2023-11-14 09:54:46 +0800

    test

--- inv/Demos/randomgenerate.py
+++ inv/Demos/randomgenerate.py
@@ -1,38 +1,42 @@
+import tkinter as tk
+from tkinter import messagebox
 import random
 
-def random_grouping(people, num_groups):
-    # 首先，将人员列表随机排序
-    random.shuffle(people)
+class RandomSelectionApp:
+    def __init__(self, master):
+        self.master = master
+        self.master.title("随机选择程序")
 
-    # 计算每组的平均人数和余下的人数
-    avg_group_size = len(people) // num_groups
-    remaining_people = len(people) % num_groups
+        self.machine_list = ["SpiderPi1", "SpiderPi2", "JetHexe", "Jettank", "JetRover"]
+        self.group_list = ["Van", "Genshin", "原神", "老八", "二刺螈"]
 
-    # 初始化分组
-    groups = []
+        self.selections = {group: None for group in self.group_list}
 
-    # 循环创建每个组
-    start_index = 0
-    for i in range(num_groups):
-        # 计算当前组的人数
-        group_size = avg_group_size + (1 if i < remaining_people else 0)
+        self.machine_label = tk.Label(master, text="选择机器:")
+        self.machine_label.pack()
 
-        # 获取当前组的人员并添加到分组列表中
-        group = people[start_index:start_index + group_size]
-        groups.append(group)
+        self.group_label = tk.Label(master, text="选择组:")
+        self.group_label.pack()
 
-        # 更新下一个组的起始索引
-        start_index += group_size
+        self.random_button = tk.Button(master, text="随机选择", command=self.random_selection)
+        self.random_button.pack()
 
-    return groups
+    def random_selection(self):
+        available_groups = [group for group in self.group_list if self.selections[group] is None]
 
-# 测试
-people = list(range(1, 12))  # 生成1到11的人员列表
-num_groups = 5
-result = random_grouping(people, num_groups)
-name_player = []
-name_group = []
+        if not available_groups:
+            messagebox.showwarning("警告", "每个组都已经选择了机器！")
+            return
 
-# 打印结果
-for i, group in enumerate(result):
-    print(f"Group {name_group[i + 1]}: {name_player[group]}")
+        selected_group = random.choice(available_groups)
+        selected_machine = random.choice(self.machine_list)
+
+        self.selections[selected_group] = selected_machine
+        self.machine_list.remove(selected_machine)
+
+        messagebox.showinfo("随机选择结果", f"{selected_group}组选择了机器{selected_machine}")
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = RandomSelectionApp(root)
+    root.mainloop()
